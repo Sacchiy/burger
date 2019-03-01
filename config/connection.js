7a601797cb37c0519864c8e339f442230f3ef62a
Sachiko Yamasaki <sachiko6248@gmail.com>
2019-02-28 20:26:02 -0800

    Herok deployment issue fix

--- config/connection.js
+++ config/connection.js
@@ -8,14 +8,15 @@ var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL){
   // Database is JawsDB on Heroku
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection =
+mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     // Database is local
     connection = mysql.createConnection({
         port: 3306,
         host: 'localhost',
         user: 'root',
-        password: "",
+        password: "Kona2014",
         database: "burgers_db"
     })
 };
